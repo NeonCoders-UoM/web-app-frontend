0e9747f2c129671610178adbd5b96ba40af3634d
B. G. M. Prabuddhi <bgm.prabuddhi@gmail.com>
2025-04-27 21:14:02 +0530

    Add Edit Vehicle Page

--- admin-frontend/src/app/vehicle/page.tsx
+++ admin-frontend/src/app/vehicle/page.tsx
@@ -152,9 +152,9 @@ const VehiclesPage = () => {
               value={clientFilter}
               onChange={(e) => setClientFilter(e.target.value)}
             >
-              <option value="All Clients">All Clients</option>
-              <option value="Active Clients">Active Clients</option>
-              <option value="Inactive Clients">Inactive Clients</option>
+              <option value="All All Vehicles">All Vehicles</option>
+              <option value="Active All Vehicles">Active Vehicles</option>
+              <option value="Inactive All Vehicles">Inactive Vehicles</option>
             </select>
             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-neutral-400">
               <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
